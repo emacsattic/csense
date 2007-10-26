34b823b0132bd8d4139ba199e76b00c240becbe5
tpatro <tpatro@4d0725c7-d73b-0410-baf4-634b9d35628e>
2007-10-26 05:51:17 +0000

    Added tooltip and line wrapping.
    git-svn-id: http://csense.googlecode.com/svn/trunk@52 4d0725c7-d73b-0410-baf4-634b9d35628e

--- csense.el
+++ csense.el
@@ -42,7 +42,12 @@ must be a plist with the follwing values:")
 
 (make-variable-buffer-local 'csense-completion-function)
 
+
+(defvar csense-max-tooltip-line-length 70
+  "Maximum length of lines in tooltips.")
+
 ;;;----------------------------------------------------------------------------
+
 (defun csense-setup ()
   "Setup Code Sense for the current buffer."
   (interactive)
@@ -60,9 +65,87 @@ must be a plist with the follwing values:")
                  (eq char-syntax-before ?_))
              (or (eq char-syntax-after ?w)
                  (eq char-syntax-after ?_)))
-        (pp (funcall csense-information-function))
+        (let* ((info (funcall csense-information-function))
+               (doc (plist-get info 'doc)))
+          (if doc
+              (csense-show-popup-help (csense-wrap-text doc))
+            (pp info)))
 
       (pp (funcall csense-completion-function)))))
+
+
+(defun csense-show-popup-help (message)
+  "Show MESSAGE in popup at point."
+  (let* ((old-propertize (symbol-function 'propertize))
+         (x-max-tooltip-size '(120 . 40))
+         (lines (split-string message "\n"))
+         (tooltip-width (* (frame-char-width)
+                           (apply 'max (mapcar 'length lines))))
+         (tooltip-height (* (frame-char-height) (min (length lines)
+                                                     (cdr x-max-tooltip-size))))
+         (xy (csense-calculate-popup-position tooltip-height tooltip-width))
+         (tooltip-frame-parameters (append `((left . ,(car xy))
+                                             (top . ,(cdr xy)))
+                                           tooltip-frame-parameters)))
+
+    ;; move the mouse cursor from the way
+    (set-mouse-position (selected-frame) 0 100)
+
+    ;; the definition of `propertize' is substituted with a dummy
+    ;; function temporarily, so that tooltip-show doesn't override the
+    ;; properties of msg
+    (fset 'propertize (lambda (string &rest properties)
+                        string))
+    (unwind-protect
+        (tooltip-show message)
+      (fset 'propertize old-propertize))))
+
+
+(defun csense-calculate-popup-position (height width)
+  "Calculate pixel position of popup at point with size HEIGHT
+and WIDTH in characters."
+  (let* ((point-pos (posn-at-point))
+         (point-xy (posn-x-y point-pos))
+         (x (let ((x (+ (car point-xy) (frame-parameter nil 'left))))
+              (if (> (+ x width) (x-display-pixel-width))
+                  (- (x-display-pixel-width) width 10)
+                x)))
+         (y (let* ((point-y (+ (cdr point-xy) (frame-parameter nil 'top)))
+                   (y (- point-y height)))
+              (if (< y 0)
+                  (+ point-y (* 4 (frame-char-height)))
+                y))))
+    (cons x y)))
+
+
+
+(defun csense-wrap-text (text)
+  "Wrap text if some of its lines are longer than
+`csense-max-tooltip-line-length'."
+  (let ((count 0)
+        (pos 0)
+        prevspace)
+    (while (< pos (length text))
+      (let ((char (aref text pos)))
+        (cond ((= char ?\n)
+               (setq count 0))
+              ((= char ? )
+               (if (< count csense-max-tooltip-line-length)
+                   (progn (setq prevspace pos)
+                          (incf count))
+
+                 ;; insert newline
+                 (if prevspace
+                     (progn (aset text prevspace ?\n)
+                            (setq count (- pos prevspace)))
+                   (aset text pos ?\n)
+                   (setq count 0))
+
+                 (setq prevspace nil)))
+              (t
+               (incf count)))
+        (incf pos))))
+  text)
 
 
 (provide 'csense)
